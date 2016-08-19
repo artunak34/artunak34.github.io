6d6d5df8b8ce5821f8283d04993b7543a1d78e41
artunak34 <aa4543@bard.edu>
2016-08-19 15:20:59 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,9 +4,9 @@
  * L&T quote generator
  */
 
-var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
+var things = ["Chaos", "Art", "Disaster", "Theater", "Geometry", "Corruption"];
 var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
-var castles = ["museums", "bridges", "cities"]
+var castles = ["arsenals", "bridges", "forts"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
 
